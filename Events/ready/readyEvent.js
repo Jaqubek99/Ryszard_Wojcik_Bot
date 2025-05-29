0f7dec41b6b738f4900cb34def4bab941015bc6f
Puchatek <cocietointeresuje>
2025-05-29 11:50:31 +0200

    Added package.json and ended onReady event handler

--- Events/ready/readyEvent.js
+++ Events/ready/readyEvent.js
@@ -1,5 +1,24 @@
 export default async (client) => {
   try {
+    const activeGuildList = client.guilds.cache;
+
+    // logging all server that bot works in
+    activeGuildList.forEach((guild) => {
+      console.log(
+        "\x1b[32m%s\x1b[0m",
+        `🤖 ${client.user.tag} bot started  on ${guild.name} server`
+      );
+    });
+
+    client.user.setPresence({
+      activies: [
+        {
+          name: "5 telefonów w ciagu minuty",
+          type: 2,
+        },
+      ],
+      status: "dnd,",
+    });
   } catch (error) {
     console.log(`Bot napotkał problem przy startowaniu | ${error}`);
   }
